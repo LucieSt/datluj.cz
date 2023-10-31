20d2ea4f0af93316e65fa02db5fd91e001546542
LucieSt <strakova.91@gmail.com>
2023-10-31 23:39:22 +0100

    finished

--- src/components/Stage/index.jsx
+++ src/components/Stage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Wordbox from '../Wordbox';
 import wordList from '../../word-list';
 import './style.css';
@@ -18,26 +18,83 @@ const generateWord = (size) => {
 };
  
 const Stage = () => {
-  const [words, setWords] = useState([generateWord(6), generateWord(6), generateWord(6), generateWord(6)]);
+  const [wordsNumber, setWordsNumber] = useState(3);
+  const [words, setWords] = useState([generateWord(3), generateWord(3), generateWord(3), generateWord(3)]);
   const [writing, setWriting] = useState('');
+  const [mistakes, setMistakes] = useState(0);
+  const [wordsFinished, setWordsFinished] = useState(5)
+  const [startGame, setStartGame] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(60)
+  const [text, setText] = useState(false)
+
+  useEffect(() => {
+    let timerId;
+    if (startGame) {
+      const countdown = () => {
+        if (timeLeft > 0) {
+          setTimeLeft(timeLeft - 1);
+        } else {
+          setText(true);
+          clearInterval(timerId);
+          setStartGame(false);
+          setTimeLeft(60);
+        }
+      };
+
+      timerId = setInterval(countdown, 1000);
+    }
+
+    return () => clearInterval(timerId);
+  }, [startGame, timeLeft]);
 
   const handleFinish = () => {
+    if (wordsFinished%5 == 0 && wordsNumber < 20) {
+      setWordsNumber(wordsNumber+1)
+    }
+    setWordsFinished((prevWordsFinished) => prevWordsFinished + 1)
     setWords([]);
-    setWords([...words.slice(1), generateWord(6)]);
+    setWords([...words.slice(1), generateWord(wordsNumber)]);
   }
 
   const handleWritingChange = (newText) => {
     setWriting(newText)
   };
 
+  const handleMistakeChange = () => {
+    setMistakes((prevMistakes) => prevMistakes + 1);
+  }
+
   return (
     <div className="stage">
-      {/* <div className="stage__mistakes">Chyb: 0</div> */}
-      <div className="stage__words">
-        {words.map((word, index) => <Wordbox word={word} key={word} onFinish={handleFinish} active={index === 0} value={writing} onChange={handleWritingChange}/>)}
-      </div>
+      {!startGame &&
+        <div>
+          <h1 className="stage__headline">Magicky psaci stroj</h1>
+          <p>Zapni si zvuk a pro start hry klikni na tlacitko:<br/><br/>Mas 1 minutu!</p>
+          <button className="stage__button" onClick={() => {
+              setText(false);
+              setWriting('');
+              setMistakes(0);
+              setStartGame(true);
+            }}>ZACIT HRU
+          </button>
+        </div>
+      }
+      {startGame && <div className="stage__words">
+        {words.map((word, index) => <Wordbox
+        word={word}
+        key={word}
+        onFinish={handleFinish}
+        active={index === 0}
+        text={writing}
+        onTextChange={handleWritingChange}
+        onMistakeChange={handleMistakeChange}
+        />)}
+      </div>}
       <div className='stage__paper'>
-        <p>{writing}</p>
+        <p className='stage__paper_mistakes'>pocet chyb: {mistakes}</p>
+        <p className='stage__paper_text'>{writing}</p>
+        {startGame && <div className="stage__counter">00 : {timeLeft}</div>}
+        {text && <p className='Stage__paper_outro'>Skvele! Chyb jsi udelal {mistakes}, to neni spatne!!</p>}
       </div>
     </div>
   );
--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -2,23 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { playSound } from '../../sound-effect';
 import './style.css';
 
-const Wordbox = ({ word, onFinish, active, value, onChange }) => {
+const Wordbox = ({ word, onFinish, active, text, onTextChange, onMistakeChange }) => {
   const [lettersLeft, setLettersLeft] = useState(word);
   const [mistake, setMistake] = useState(false);
   
   const handleKeyUp = (e) => {
-    onChange(value + e.key);
+    onTextChange(text + e.key);
     if (e.key === lettersLeft[0]) {
       playSound('typeSound');
       setMistake(false);
       setLettersLeft((prevValue) => prevValue.slice(1))
       if (lettersLeft.length === 1) {
         playSound('doneSound');
-        onChange(value + e.key + ' ')
+        onTextChange(text + e.key + ' ')
         onFinish();
       };
     } else {
       playSound('wrongTypeSound');
+      onMistakeChange()
       setMistake(true);
     }
   }
